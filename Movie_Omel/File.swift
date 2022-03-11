7f9da838e14e0901ff7eb9d784337ac53702978d
NazarVik <Nazarov_dev@icloud.com>
2022-03-11 18:40:37 +0300

    working

---
+++ Movie_Omel/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Movie_Omel
+//
+//  Created by Виктор Назаров on 11.03.22.
+//
+
+import Foundation
---
+++ Movie_Omel/GenreModel.swift
@@ -0,0 +1,18 @@
+//
+//  GenreModel.swift
+//  Movie_Omel
+//
+//  Created by Виктор Назаров on 4.03.22.
+//
+
+import Foundation
+
+struct GenreModel {
+    var genres : [Genres]
+    
+}
+
+struct Genres {
+    let id: Int
+    let name: String
+}
